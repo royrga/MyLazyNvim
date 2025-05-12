0d4408501d2bc1568a1d007f4c59024b1ea8765d
royrga <royrga@gmail.com>
2025-05-11 18:02:18 -0600

    modify wrap_spell autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- wrap and check for spell in text filetypes
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = false
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "es"
+  end,
+})
